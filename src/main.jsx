075797cb025a2518e237086047893904775f8936
jeeyalal <harshmishra6927@gmail.com>
2025-12-02 17:12:56 +0530

    added react tostify

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,28 @@ import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext.jsx";
 import { SubscriptionProvider } from './context/SubscriptionContext.jsx';
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <CartProvider>
-      <SubscriptionProvider>
-        <App />
-      </SubscriptionProvider>
-    </CartProvider>
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <CartProvider>
+        <SubscriptionProvider>
+
+          <App />
+
+          {/* 🔥 Toasts now appear in top-right */}
+          <ToastContainer
+            position="top-right"
+            theme="colored"
+            autoClose={2000}
+            pauseOnHover={false}
+            newestOnTop={true}
+          />
+
+        </SubscriptionProvider>
+      </CartProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
