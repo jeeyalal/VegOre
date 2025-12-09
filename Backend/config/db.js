53e508929159a29006c20031b6bff2c90fb788a7
jeeyalal <harshmishra6927@gmail.com>
2025-12-09 11:34:39 +0530

    add env examples for collaborators

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,8 +1,23 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("✅ MongoDB Connected Successfully");
+//   } catch (error) {
+//     console.log("❌ MongoDB Connection Error:", error.message);
+//     process.exit(1);
+//   }
+// };
+
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    console.log("DEBUG MONGO_URI =>", process.env.MONGO_URI); // ✅ must print your URI
+
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
     console.log("❌ MongoDB Connection Error:", error.message);
